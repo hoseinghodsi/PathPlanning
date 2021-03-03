96840e5c7355a6bd491ba99c6a9d0b62259d655c
Hossein Ghodsi <hoseinghodsi84@gmail.com>
2021-03-02 23:07:43 -0500

    first commit

---
+++ build/thirdparty/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hossein/Udacity/PathPlanning/thirdparty/googletest
+# Build directory: /home/hossein/Udacity/PathPlanning/build/thirdparty/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hossein/Udacity/PathPlanning/thirdparty/googletest/googlemock
+# Build directory: /home/hossein/Udacity/PathPlanning/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
